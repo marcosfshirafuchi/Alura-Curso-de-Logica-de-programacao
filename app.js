8a6381a55fa386f3d36d18a620983ddffab95154
Marcos Ferreira Shirafuchi <marcosfshirafuchi@gmail.com>
2024-11-04 09:37:42 -0300

    02. Condicionais e concatenação

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 alert("Boas vindas ao jogo do número secreto");
-let numeroSecreto = 29;
+let numeroSecreto = 9;
 //Armazena a variavel digitada no prompt
+console.log(numeroSecreto);
 let chute = prompt("Escolha um número entre 1 e 10");
 //Verifica se o chute é igual ao número secreto
+// se chute for igual ao número secreto
 if(chute == numeroSecreto){
-    console.log("Isso ai! Você descobriu o número secreto (5)");
+    alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto}`);
+}else{
+    alert("Você errou :(")
 }
---
+++ desafio02.js
@@ -0,0 +1,8 @@
+//2 - Verifique se um número digitado pelo usuário é positivo ou negativo. Mostre um alerta informando.
+
+let numeroDigitado = prompt("Digite um número: ");
+if(numeroDigitado>0){
+    alert("Número Positivo!");
+}else{
+    alert("Número Negativo!");
+}
